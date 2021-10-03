ba00d734366f7be01bd5abecdf5219d4497e2654
Andrey Skrypka <skrypka1203@gmail.com>
2021-10-03 13:42:48 +0300

    1st task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,11 @@
 function sumSalary(salaries) {
-
+  let sum = 0;
+  for (let key in salaries) {
+    if (typeof salaries[key] === 'number' && !isNaN(salaries[key]) && salaries[key] != Infinity && salaries[key] != -Infinity) {
+      sum += salaries[key];
+    } else if (salaries[key] === 0){
+      return 0;
+    }
+  }
+  return sum
 }
