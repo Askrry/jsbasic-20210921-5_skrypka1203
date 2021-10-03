e038514777c67184ddddbb2f60e7fd35bbaf6669
Andrey Skrypka <skrypka1203@gmail.com>
2021-10-03 20:40:45 +0300

    1st task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,15 @@
 function factorial(n) {
-  // ваш код...
+  for (n=0; n = 0; !n){
+
+  }
+  for (n=0; n = 1; !n){
+
+  }
+  for (n=0; n = 3; !n){
+
+  }
+  for (n=0; n = 5; !n){
+
+  }
+  return n
 }
