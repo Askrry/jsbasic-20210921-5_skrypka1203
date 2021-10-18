d3609a0fa2227dc41b243a6beee94e330615d919
Andrey Skrypka <skrypka1203@gmail.com>
2021-10-18 17:58:19 +0300

    3-4 module

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,8 @@
 function showSalary(users, age) {
-  // ваш код...
-}
+
+  let ageFilter = users.filter(item => (item["age"] <= age ));
+  let ageAndSalary = ageFilter.reduce((item, current) => item + current["name"] + ", " + current["balance"] + "\n", "");
+
+  return ageAndSalary.slice(0,-1);
+
+};
